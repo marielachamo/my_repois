128273a142727f5097e144fea2d54cfe4dcd8d00
marielachamo <mchamopaco@gmail.com>
2025-04-22 19:40:20 -0400

    ignorar errores de eslint durante deploy

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,10 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
 export default nextConfig;
+
